ea396d50329cbf7b32bc23c01de5908639df1b22
Sidelnikov Roman <79612731370@yandex.ru>
2021-11-10 23:36:50 +0300

    Переименованы комнаты

--- Assets/NPP-Map/Scripts/Map.cs
+++ Assets/NPP-Map/Scripts/Map.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace NPPMap
@@ -8,5 +9,10 @@ namespace NPPMap
     {
         [SerializeField] private Sprite map;
         [SerializeField] private List<RoomInformation> rooms;
+
+        public IReadOnlyList<RoomInformation> GetRooms() => rooms;
+
+        public IReadOnlyList<MachineInformation> GetMachines() =>
+            GetRooms().SelectMany(information => information.MachinesInformation).ToList();
     }
 }
