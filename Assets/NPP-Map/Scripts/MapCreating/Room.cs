842e9660708ed413a08e7654d66d317d1dada167
Sidelnikov Roman <79612731370@yandex.ru>
2022-03-30 00:31:14 +0300

    Добавлено сохранение нарисованной карты

--- Assets/NPP-Map/Scripts/MapCreating/Room.cs
+++ Assets/NPP-Map/Scripts/MapCreating/Room.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using mattatz.Triangulation2DSystem;
 using UnityEngine;
 
@@ -7,20 +9,59 @@ namespace NPPMap.MapCreating
     public class Room : MonoBehaviour
     {
         [SerializeField] private MeshFilter meshFilter;
+        [SerializeField] private MeshCollider meshCollider;
         [SerializeField] private LineRenderer lineRenderer;
 
-        private Vector2[] _points;
+        public RoomData Data { get; private set; }
 
-        public void Init(Vector2[] roomSketchPoints)
+        public void Init(RoomData data)
         {
-            _points = roomSketchPoints;
+            Data = data;
+            Vector2[] roomPoints = data.Points.ToArray();
 
-            var triangulation = new Triangulation2D(Polygon2D.Contour(roomSketchPoints));
-            meshFilter.mesh = triangulation.Build();
+            var triangulation = new Triangulation2D(Polygon2D.Contour(roomPoints));
+            Mesh mesh = triangulation.Build();
+            meshFilter.mesh = mesh;
+            meshCollider.sharedMesh = mesh;
 
             lineRenderer.loop = true;
-            lineRenderer.positionCount = roomSketchPoints.Length;
-            lineRenderer.SetPositions(roomSketchPoints.Select(point => (Vector3)point).ToArray());
+            lineRenderer.positionCount = roomPoints.Length;
+
+            Vector3[] positions = roomPoints.Select(PointsForLine).ToArray();
+            lineRenderer.SetPositions(positions);
+
+            Vector3 PointsForLine(Vector2 point) => (Vector3)point + Vector3.forward * -0.01f;
         }
+    }
+
+    public class RoomData
+    {
+        public string Description { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Vector2> Points { get; set; } = new List<Vector2>();
+
+        public List<RoomObjectData> RoomObjects { get; set; } = new List<RoomObjectData>();
+
+        public Vector3 RoomPosition { get; set; }
+
+        public RoomData()
+        {
+        }
+
+        public RoomData(List<Vector2> roomPoints, Vector3 roomPosition)
+        {
+            if (roomPoints == null || roomPoints.Count < 3)
+                throw new ArgumentException("Incorrect room, minimum 3 points");
+            Points = roomPoints;
+            RoomPosition = roomPosition;
+        }
+    }
+
+    public class RoomObjectData
+    {
+        public string Description { get; set; }
+        public string Name { get; set; }
     }
 }
--- Assets/NPP-Map/Scripts/MapCreating/StateMachine/CreatingRoomState.cs
+++ Assets/NPP-Map/Scripts/MapCreating/StateMachine/CreatingRoomState.cs
@@ -1,4 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using SimpleFileBrowser;
 using UnityEngine;
 
 namespace NPPMap.MapCreating
@@ -14,6 +18,7 @@ namespace NPPMap.MapCreating
 
         private RoomSketch _roomSketch;
         private readonly List<Room> _rooms = new List<Room>();
+        private float _offsetZ = 10;
 
         private void Start()
         {
@@ -22,9 +27,15 @@ namespace NPPMap.MapCreating
             simpleWallVisualizer.Init(roomPainter, _roomSketch.TryGetLastPoint);
         }
 
-        public void Clear()
+        public void ClearRoom()
         {
             _roomSketch.Clear();
+        }
+
+        public void ClearMap()
+        {
+            _rooms.ForEach(room => Destroy(room.gameObject));
+            _rooms.Clear();
         }
 
         public void Disable()
@@ -32,6 +43,10 @@ namespace NPPMap.MapCreating
             _roomSketch.Clear();
             roomPainter.Disable();
             simpleWallVisualizer.Disable();
+
+            ClearRoom();
+            ClearMap();
+            
             roomPainter.OnSetPoint -= _roomSketch.AddPoint;
             createRoomUI.SetActive(false);
         }
@@ -44,7 +59,17 @@ namespace NPPMap.MapCreating
             createRoomUI.SetActive(true);
         }
 
-        public void Save()
+        public void LoadMap()
+        {
+            FileBrowser.ShowLoadDialog(paths => LoadMap(paths[0]), null, FileBrowser.PickMode.Files);
+        }
+
+        public void SaveMap()
+        {
+            FileBrowser.ShowSaveDialog(paths => SaveMap(paths[0]), null, FileBrowser.PickMode.Files);
+        }
+
+        public void SaveRoom()
         {
             _rooms.Add(CreateRoom(_roomSketch.Points));
             _roomSketch.Clear();
@@ -57,9 +82,61 @@ namespace NPPMap.MapCreating
 
         private Room CreateRoom(Vector2[] roomSketchPoints)
         {
-            Room room = Instantiate(roomPrefab, parentForRooms);
-            room.Init(roomSketchPoints);
+            Vector2 center = FindCenter(roomSketchPoints);
+            Vector3 roomPosition = (Vector3)center + Vector3.forward * _offsetZ;
+            var roomData = new RoomData(roomSketchPoints.Select(point => point - center).ToList(), roomPosition);
+
+            return CreateRoom(roomData);
+        }
+
+        private Room CreateRoom(RoomData roomData)
+        {
+            Room room = Instantiate(roomPrefab, roomData.RoomPosition, Quaternion.identity, parentForRooms);
+            room.Init(roomData);
             return room;
+        }
+
+        private Vector2 FindCenter(Vector2[] roomSketchPoints)
+        {
+            Vector2 max = Vector2.negativeInfinity;
+            Vector2 min = Vector2.positiveInfinity;
+            foreach (Vector2 point in roomSketchPoints)
+            {
+                if (point.x > max.x)
+                    max.x = point.x;
+                if (point.x < min.x)
+                    min.x = point.x;
+
+                if (point.y > max.y)
+                    max.y = point.y;
+                if (point.y < min.y)
+                    min.y = point.y;
+            }
+
+            Vector2 center = min + (max - min) / 2;
+            return center;
+        }
+
+        private void LoadMap(string path)
+        {
+            string json = File.ReadAllText(path);
+
+            if (string.IsNullOrEmpty(json))
+                return;
+
+            var rooms = JsonConvert.DeserializeObject<List<RoomData>>(json);
+            if (rooms == null)
+                return;
+
+            ClearMap();
+            foreach (RoomData roomData in rooms)
+                _rooms.Add(CreateRoom(roomData));
+        }
+
+        private void SaveMap(string path)
+        {
+            string json = JsonConvert.SerializeObject(_rooms.Select(room => room.Data).ToList());
+            File.WriteAllText(path, json);
         }
     }
 }
